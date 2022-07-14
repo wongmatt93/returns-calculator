b73a4946a9aeff1e0082c605cddaaa125f994aea
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-07-13 23:09:36 -0400

    started modal, still need to style it

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+//array of objects to hold stocks
 const portfolio = [
   {
     ticker: "AAPL",
@@ -9,12 +10,31 @@ const portfolio = [
   },
 ];
 
+const openOptionsPositions = [
+  {
+    ticker: "AAPL",
+    expiration: "8-19-22",
+    strike: 135,
+    callPut: "Put",
+    premium: 250,
+    buySell: "Sell",
+  },
+];
+
+//declaring variables
 const portfolioList = document.querySelector(".portfolio-list");
 const addStockForm = document.querySelector(".add-stock-form");
 const deleteStock = document.querySelector(".fa-trash-can");
 const finalCostBasis = document.querySelector(".total-cost-basis");
 const finalCashReturn = document.querySelector(".total-cash-return");
+const stockFormModal = document.querySelector(".stock-detail-modal-container");
+const detailHeader = document.querySelector(".detail-header");
+const detailQuantity = document.querySelector(".detail-quantity");
+const detailCostBasis = document.querySelector(".detail-cost-basis");
+const detailReturns = document.querySelector(".detail-returns");
+const detailActiveOptions = document.querySelector(".detail-active-options");
 
+//print portfolio to homepage function
 const printPortfolio = (portfolio) => {
   portfolioList.textContent = "";
   let totalCostBasis = 0;
@@ -23,6 +43,7 @@ const printPortfolio = (portfolio) => {
   portfolio.forEach((stock, index) => {
     const newStock = document.createElement("tr");
     const ticker = document.createElement("td");
+    const tickerLink = document.createElement("a");
     const quantity = document.createElement("td");
     const openOptions = document.createElement("td");
     const buyOptions = document.createElement("button");
@@ -33,17 +54,19 @@ const printPortfolio = (portfolio) => {
     const removeStock = document.createElement("i");
 
     newStock.classList.add("individual-stock");
+    tickerLink.classList.add("stock-link");
     openOptions.classList.add("open-options");
     buyOptions.classList.add("buy-option");
     sellOptions.classList.add("sell-option");
     removeStock.classList.add("fa-solid", "fa-trash-can");
 
+    tickerLink.setAttribute("href", `?stock=${stock.ticker}`);
     openOptions.setAttribute("data-stock", stock.ticker);
     buyOptions.setAttribute("data-buysell", "buy");
     sellOptions.setAttribute("data-buysell", "sell");
     removeStock.setAttribute("data-index", index);
 
-    ticker.textContent = stock.ticker;
+    tickerLink.textContent = stock.ticker;
     quantity.textContent = stock.quantity
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -58,6 +81,7 @@ const printPortfolio = (portfolio) => {
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 
+    ticker.append(tickerLink);
     openOptions.append(buyOptions, sellOptions);
     removeStockItem.append(removeStock);
 
@@ -85,6 +109,29 @@ const printPortfolio = (portfolio) => {
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 };
 
+//print options in detail modal function
+const printOptions = (options, ticker) => {
+  detailActiveOptions.textContent = "";
+  options.forEach((option) => {
+    if (option.ticker == ticker) {
+      const newOption = document.createElement("li");
+      const expiration = document.createElement("p");
+      const strike = document.createElement("p");
+      const callPut = document.createElement("p");
+      const premium = document.createElement("p");
+
+      expiration.textContent = option.expiration;
+      strike.textContent = option.strike;
+      callPut.textContent = option.callPut;
+      premium.textContent = option.premium;
+
+      newOption.append(expiration, strike, callPut, premium);
+      detailActiveOptions.append(newOption);
+    }
+  });
+};
+
+//add a stock to portfolio
 addStockForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const ticker = document.querySelector("#ticker").value;
@@ -105,11 +152,36 @@ addStockForm.addEventListener("submit", (event) => {
 });
 
 portfolioList.addEventListener("click", (event) => {
+  //delete a stock from portfolio
   if (event.target.classList.contains("fa-trash-can")) {
     const index = event.target.getAttribute("data-index");
     portfolio.splice(index, 1);
     printPortfolio(portfolio);
   }
+  //open stock modal for more details
+  if (event.target.classList.contains("stock-link")) {
+    event.preventDefault();
+    const ticker = event.target.textContent;
+    stockFormModal.classList.remove("toggle-modal");
+    detailHeader.textContent = ticker;
+
+    const stock = portfolio.find((stock) => {
+      return stock.ticker == ticker;
+    });
+
+    detailQuantity.textContent = stock.quantity;
+    detailCostBasis.textContent = stock.costBasis;
+    detailReturns.textContent = stock.cashReturn;
+    printOptions(openOptionsPositions, ticker);
+  }
 });
 
+//close stock modal after opening
+stockFormModal.addEventListener("click", (event) => {
+  if (event.target.classList.contains("stock-detail-modal-container")) {
+    stockFormModal.classList.add("toggle-modal");
+  }
+});
+
+//print portfolio to homepage when first loading
 printPortfolio(portfolio);
