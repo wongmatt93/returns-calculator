d4ad52433a6a1b24cfa4760997478d439bebde74
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-07-16 22:25:03 -0400

    add buttons to modal

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,12 @@ const portfolio = [
   {
     ticker: "AAPL",
     quantity: 0,
-    costBasis: 28000,
-    cashReturn: 750,
+    costBasis: 28600,
+    cashReturn: 1500,
   },
 ];
 
-const openOptionsPositions = [
+const OptionsPositions = [
   {
     ticker: "AAPL",
     expiration: "8-19-22",
@@ -16,6 +16,7 @@ const openOptionsPositions = [
     callPut: "Put",
     premium: 250,
     buySell: "Sell",
+    active: true,
   },
 ];
 
@@ -31,7 +32,7 @@ const detailHeader = document.querySelector(".detail-header");
 const detailQuantity = document.querySelector(".detail-quantity");
 const detailCostBasis = document.querySelector(".detail-cost-basis");
 const detailReturns = document.querySelector(".detail-returns");
-const detailActiveOptions = document.querySelector(".detail-active-options");
+const detailActiveOptions = document.querySelector(".detail-all-options");
 
 //print portfolio to homepage function
 const printPortfolio = (portfolio) => {
@@ -51,11 +52,17 @@ const printPortfolio = (portfolio) => {
     const removeStockItem = document.createElement("td");
     const removeStock = document.createElement("i");
 
-    const openBuyOptions = openOptionsPositions.filter(
-      (option) => option.buySell == "Buy"
+    const openBuyOptions = OptionsPositions.filter(
+      (option) =>
+        option.buySell == "Buy" &&
+        option.ticker == stock.ticker &&
+        option.active
     ).length;
-    const openSellOptions = openOptionsPositions.filter(
-      (option) => option.buySell == "Sell"
+    const openSellOptions = OptionsPositions.filter(
+      (option) =>
+        option.buySell == "Sell" &&
+        option.ticker == stock.ticker &&
+        option.active
     ).length;
 
     newStock.classList.add("individual-stock");
@@ -108,7 +115,7 @@ const printPortfolio = (portfolio) => {
     .toFixed(2)
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
-  if (totalCostBasis != 0) {
+  if (totalCostBasis.textContent != "$0.00") {
     totalPercentReturn.textContent = `${(
       (stockCashReturn / stockCostBasis) *
       100
@@ -133,9 +140,15 @@ const printOptions = (options, ticker) => {
       const premium = document.createElement("p");
 
       expiration.textContent = option.expiration;
-      strike.textContent = option.strike;
+      strike.textContent = `$${option.strike
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
       callPut.textContent = option.callPut;
-      premium.textContent = option.premium;
+      premium.textContent = `$${option.premium
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 
       newOption.append(expiration, strike, callPut, premium);
       detailActiveOptions.append(newOption);
@@ -149,19 +162,22 @@ addStockForm.addEventListener("submit", (event) => {
   const ticker = document.querySelector("#ticker").value;
   const costBasis = parseInt(document.querySelector("#cost-basis").value);
 
-  const newStock = {
-    ticker,
-    quantity: 0,
-    buyOptions: 0,
-    sellOptions: 0,
-    costBasis,
-    cashReturn: 0,
-  };
+  if (portfolio.some((stock) => stock.ticker == ticker)) {
+    alert("You've already added this stock");
+  } else {
+    const newStock = {
+      ticker,
+      quantity: 0,
+      costBasis,
+      cashReturn: 0,
+    };
 
-  portfolio.push(newStock);
-  printPortfolio(portfolio);
+    portfolio.push(newStock);
+    printPortfolio(portfolio);
+  }
 });
 
+//multiple events, but both are included in same event listener
 portfolioList.addEventListener("click", (event) => {
   //delete a stock from portfolio
   if (event.target.classList.contains("fa-trash-can")) {
@@ -180,10 +196,18 @@ portfolioList.addEventListener("click", (event) => {
       return stock.ticker == ticker;
     });
 
-    detailQuantity.textContent = stock.quantity;
-    detailCostBasis.textContent = stock.costBasis;
-    detailReturns.textContent = stock.cashReturn;
-    printOptions(openOptionsPositions, ticker);
+    detailQuantity.textContent = `Number of Shares: ${stock.quantity
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    detailCostBasis.textContent = `Cost Basis: $${stock.costBasis
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    detailReturns.textContent = `Cash Return: $${stock.cashReturn
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    printOptions(OptionsPositions, ticker);
   }
 });
 
